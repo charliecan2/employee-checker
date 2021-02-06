0c2519901510c51d9d3191bb20f659538bb050ff
ccantu20 <carloscq920@gmail.com>
2021-02-06 00:00:46 -0600

    Incorporated a promise that will run functions based on user input by using if/else if statements. Functions will be created down the line

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ function init(){inquirer.prompt([
     {
         type: 'list',
         message: "What would you like to do?",
-        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Role", "Add Employee", "Add Department", "Add Role", "Update Employee Role"]
+        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Role", "Add Employee", "Add Department", "Add Role", "Update Employee Role"],
+        name: 'action'
     }
-])}
+]).then((response) => {
+    if (response.action === "View All Employees"){
+
+    }
+    else if (response.action === "View All Employees By Department"){
+
+    }
+    else if (response.action === "View All Employees By Role"){
+
+    }
+    else if (response.action === "Add Employee"){
+
+    }
+    else if (response.action === "Add Department"){
+
+    }
+    else if (response.action === "Add Role"){
+
+    }
+    else if (response.action === "Update Employee Role"){
+        
+    }
+})
+}
